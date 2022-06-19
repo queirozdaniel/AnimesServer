7a69af6f4d6ea0de78d9d3d70d3dda35b521bc91
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-18 21:10:26 -0300

    refactor: Change test response

--- src/test/kotlin/com/danielqueiroz/ApplicationTest.kt
+++ src/test/kotlin/com/danielqueiroz/ApplicationTest.kt
@@ -23,7 +23,6 @@ class ApplicationTest {
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
         }
     }
 }
